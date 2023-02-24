7410ac79e679500e81dd0af6fdbd51427200538d
王福元 <wangfuyuan@nnuo.com>
2023-02-24 17:22:27 +0800

    docs: 修改文档和demo

--- dist/script/numberAnimate.js
+++ dist/script/numberAnimate.js
@@ -7,17 +7,16 @@
  * @基于jquery
  */
 
-;
 (function ($) {
   $.fn.numberAnimate = function (setting) {
     var defaults = {
       speed: 1000, //动画速度
       num: "", //初始化值
       iniAnimate: true, //是否要初始化动画效果
-      symbol: '', //默认的分割符号，千，万，千万
+      symbol: "", //默认的分割符号，千，万，千万
       dot: 0, //保留几位小数点
-      pst: "" //是否有 百分号
-    }
+      pst: "", //是否有 百分号
+    };
     //如果setting为空，就取default的值
     var setting = $.extend(defaults, setting);
 
@@ -32,7 +31,8 @@
       alert("must set a num!");
       return;
     }
-    var nHtml = '<div class="mt-number-animate-dom" data-num="{{num}}">\
+    var nHtml =
+      '<div class="mt-number-animate-dom" data-num="{{num}}">\
             <span class="mt-number-animate-span">0</span>\
             <span class="mt-number-animate-span">1</span>\
             <span class="mt-number-animate-span">2</span>\
@@ -50,74 +50,85 @@
     //数字处理
     var numToArr = function (num) {
       num = parseFloat(num).toFixed(setting.dot);
-      if (typeof (num) == 'number') {
+      if (typeof num == "number") {
         var arrStr = num.toString().split("");
       } else {
         var arrStr = num.split("");
       }
       //console.log(arrStr);
       return arrStr;
-    }
+    };
 
     //设置DOM symbol:分割符号
     var setNumDom = function (arrStr) {
       var shtml = '<div class="mt-number-animate">';
       for (var i = 0, len = arrStr.length; i < len; i++) {
-        if (i != 0 && (len - i) % 3 == 0 && setting.symbol != "" && arrStr[i] != ".") {
-          shtml += '<div class="mt-number-animate-dot">' + setting.symbol + '</div>' + nHtml.replace("{{num}}", arrStr[i]);
+        if (
+          i != 0 &&
+          (len - i) % 3 == 0 &&
+          setting.symbol != "" &&
+          arrStr[i] != "."
+        ) {
+          shtml +=
+            '<div class="mt-number-animate-dot">' +
+            setting.symbol +
+            "</div>" +
+            nHtml.replace("{{num}}", arrStr[i]);
         } else {
           shtml += nHtml.replace("{{num}}", arrStr[i]);
         }
       }
       if (setting.pst) {
-        shtml += '%</div>';
+        shtml += "%</div>";
       } else {
-        shtml += '</div>';
+        shtml += "</div>";
       }
       return shtml;
-    }
+    };
 
     //执行动画
     var runAnimate = function ($parent) {
       $parent.find(".mt-number-animate-dom").each(function () {
         var num = $(this).attr("data-num");
-        // console.log(num)
-        num = (num == "." ? 11 : num == 0 ? 10 : num);
+        num = num == "." ? 11 : num == 0 ? 10 : num;
         var spanHei = $(this).height() / 12; //11为元素个数
         var thisTop = -num * spanHei + "px";
         if (thisTop != $(this).css("top")) {
           if (setting.iniAnimate) {
             //HTML5不支持
             if (!window.applicationCache) {
-              $(this).animate({
-                top: thisTop
-              }, setting.speed);
+              $(this).animate(
+                {
+                  top: thisTop,
+                },
+                setting.speed
+              );
             } else {
               $(this).css({
-                'transform': 'translateY(' + thisTop + ')',
-                '-ms-transform': 'translateY(' + thisTop + ')',
+                transform: "translateY(" + thisTop + ")",
+                "-ms-transform": "translateY(" + thisTop + ")",
                 /* IE 9 */
-                '-moz-transform': 'translateY(' + thisTop + ')',
+                "-moz-transform": "translateY(" + thisTop + ")",
                 /* Firefox */
-                '-webkit-transform': 'translateY(' + thisTop + ')',
+                "-webkit-transform": "translateY(" + thisTop + ")",
                 /* Safari 和 Chrome */
-                '-o-transform': 'translateY(' + thisTop + ')',
-                '-ms-transition': setting.speed / 1000 + 's',
-                '-moz-transition': setting.speed / 1000 + 's',
-                '-webkit-transition': setting.speed / 1000 + 's',
-                '-o-transition': setting.speed / 1000 + 's',
-                'transition': setting.speed / 1000 + 's'
+                "-o-transform": "translateY(" + thisTop + ")",
+                "-ms-transition": setting.speed / 1000 + "s",
+                "-moz-transition": setting.speed / 1000 + "s",
+                "-webkit-transition": setting.speed / 1000 + "s",
+                "-o-transition": setting.speed / 1000 + "s",
+                transition: setting.speed / 1000 + "s",
               });
             }
           } else {
             setting.iniAnimate = true;
             $(this).css({
-              top: thisTop
+              top: thisTop,
             });
           }
         }
       });
-    }
+    };
 
     //初始化
     var init = function ($parent) {
@@ -138,9 +149,9 @@
         });
       }
       runAnimate($(this));
-    }
+    };
     //init
     init($(this));
     return this;
-  }
+  };
 })(jQuery);
